f56397573ccaeb29ac034d94d85ecb3481c13535
Lorru <plock1125@gmail.com>
2025-04-17 15:31:01 +0800

    part3:daisyUI安裝&測試功能是否正常

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -6,6 +6,13 @@
       <h1>Home Page</h1>
       <p>Welcome to the home page!</p>
     </div>
+    <button
+    class="inline-block cursor-pointer rounded-md bg-gray-800 px-4 py-3 text-center text-sm font-semibold uppercase text-white transition duration-200 ease-in-out hover:bg-gray-900">
+    Button
+    </button>
+    <button class="btn btn-primary">Button</button>
+    <button class="btn">Button</button>
+    <button class="btn w-64 rounded-full">Button</button>
 </template>
 
 <style>
